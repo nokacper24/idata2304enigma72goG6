cbd35ff870dfbfcb1ee50fb6190900abf23bfdaa
Kacper Lukasz Nowicki <kacperln@ntnu.no>
2022-09-29 14:31:09 +0200

    docs: :memo: add docs

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"time"
 )
 
 func main() {
-	
+
 	url := "129.241.152.12"
 	port := "1234"
 	message := "task"
@@ -51,10 +52,10 @@ func main() {
 }
 
 /**
- Creates a UDP connection to the server and sends the initial connection message
+Creates a UDP connection to the server and sends the initial connection message
 
- @return net.Conn - the connection to the server
- */
+@return net.Conn - the connection to the server
+*/
 func sendUDPReq(url string, port string, message string) (net.Conn, error) {
 	// Send UDP request to server
 	conn, err := net.Dial("udp", url+":"+port)
@@ -68,10 +69,17 @@ func sendUDPReq(url string, port string, message string) (net.Conn, error) {
 	return conn, nil
 }
 
+/**
+Reads the response from the server.
 
+@param net.Conn - the connection to the server
+
+@return string - the response from the server
+*/
 func readUDPResp(conn net.Conn) (string, error) {
 	// Read response from server
 	buf := make([]byte, 1024)
+	time.Sleep(1 * time.Second)
 	n, err := conn.Read(buf)
 	if err != nil {
 		return "", err
@@ -79,35 +87,39 @@ func readUDPResp(conn net.Conn) (string, error) {
 	return string(buf[:n]), nil
 }
 
+/**
+Processes the response from the server and returns the correct response.
 
+@return string - the correct response to send back to the server
+*/
 func processRespone(response string) (string, error) {
 	var responseString string
-// Process response from server
-// get the last character of the response
-punctuation := response[len(response)-1:]
+	// Process response from server
+	// get the last character of the response
+	punctuation := response[len(response)-1:]
 
-var quorsta string 
-if punctuation == "." {
-	quorsta = "statement"
-}
-if punctuation == "?" {
-	quorsta = "question"
-}
+	var quorsta string
+	if punctuation == "." {
+		quorsta = "statement"
+	}
+	if punctuation == "?" {
+		quorsta = "question"
+	}
 
-//get the amount of words in the response
-words := strings.Fields(response)
-wordCount := len(words)
+	//get the amount of words in the response
+	words := strings.Fields(response)
+	wordCount := len(words)
 
-//get the amount of characters in the response
-charCount := len(response)
+	//get the amount of characters in the response
+	charCount := len(response)
 
-if charCount == 1 {
-	wordCount = 0
-}
+	if charCount == 1 {
+		wordCount = 0
+	}
 
-wordCountString := fmt.Sprintf("%d", wordCount)
+	wordCountString := fmt.Sprintf("%d", wordCount)
 
-responseString = quorsta + " " + wordCountString
+	responseString = quorsta + " " + wordCountString
 
-return responseString, nil
+	return responseString, nil
 }
